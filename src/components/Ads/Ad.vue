24bb8022e5a0cfb059bb7f24133f0a23875ae0a8
Andrey_programmer <andrey@example.com>
2018-11-16 13:44:47 +0300

    Lesson_12 - Добавлена страница Ad

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,22 @@
     <v-container grid-list-xs>
         <v-layout row wrap>
             <v-flex xs12>
-                <h1>Ad</h1>
+                <v-card  class="elevation-10" v-for="ad of ads" :key="ad.id">
+                    <v-img
+                        :src="ad.imageSrc"
+                        height="400"
+                        >
+                    </v-img>
+                    <v-card-text class="px-5">
+                        <h1 class="text--primary">{{ad.title}}</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, nisi numquam. Exercitationem eligendi et enim.</p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="warning" flat>Edit</v-btn>
+                        <v-btn class="success">Buy</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
@@ -13,6 +28,15 @@
 export default {
     data() {
         return {
+            ads: [
+                {
+                    title: 'First ad', 
+                    description: 'I\'m description', 
+                    promo: false, 
+                    imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg', 
+                    id: '123'
+                }
+            ]
         }
     }
 }
