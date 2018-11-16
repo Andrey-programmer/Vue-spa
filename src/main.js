d1f07afbf7b627d06ac31fe5f604964d71fb23ba
Andrey_programmer <andrey@example.com>
2018-11-16 16:58:40 +0300

    Lesson_15 - Установил firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import App from './App'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import * as firebase from 'firebase'
 
 import router from './router/router'
 import store from './store/base'
@@ -11,11 +12,22 @@ Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+/* eslint-disable */
 new Vue({
   el: '#app',
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    var config = {
+      apiKey: 'AIzaSyAyVWAhtlHOMlQQ7k7YZ713NtILu-w-m9U',
+      authDomain: 'vue-spa-project.firebaseapp.com',
+      databaseURL: 'https://vue-spa-project.firebaseio.com',
+      projectId: 'vue-spa-project',
+      storageBucket: 'vue-spa-project.appspot.com',
+      messagingSenderId: '314937424136'
+    }
+      firebase.initializeApp(config)
+  }
 })
