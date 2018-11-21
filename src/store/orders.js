3a3b2172e6a0e5adc109a786da5ec29f34031c63
Andrey_programmer <andrey@example.com>
2018-11-21 17:01:10 +0300

    Lesson_27 - Загружаем заказ пользователя на сервер

--- src/store/orders.js
+++ src/store/orders.js
@@ -1,20 +1,38 @@
 /* eslint-disable */
+import * as firebase from 'firebase'
+
+class Order {
+    constructor (name, phone, adId, done = false, id = null) {
+        this.name = name,
+        this.phone = phone,
+        this.adId = adId,
+        this.done = done,
+        this.id = id
+    }
+}
+
 export default {
     state: {
         orders: []
     },
     mutations: {
-        createOrder (state, data) {
-           state.orders.push(data) 
+        loadOrders (state, data) {
+            state.orders = data
         }
     },
     actions: {
-        async createOrder ({commit}, data) {
-            await new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve()
-                }, 4000)
-            })
+        async createOrder ({commit}, {name, phone, adId, done, ownerId}) {
+            commit('clearError')
+
+            const order = new Order(name, phone, adId)
+
+            try {
+                await firebase.database().ref(`/users/${ownerId}/orders`).push(order)
+
+            } catch (error) {
+                commit('setError', error.message)
+                throw error
+            }
         }
     },
     getters: {
